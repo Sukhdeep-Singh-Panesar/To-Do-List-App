a221f04f8b0c3e751a22be2c2b54ee32b6649029
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-05-11 14:16:21 -0500

    Margin changes for Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ function Navbar() {
 
     return (
         <>
-            <nav className="bg-[#E4B9B9] w-full p-2 flex gap-6">
+            <nav className="bg-[#E4B9B9] w-full p-2 flex gap-6 m-2">
                 <div className="logo font-island bg-[#F87777] py-2 px-2 border-2 border-solid rounded-full text-4xl shadow-[-]">TH</div>
                 <h1 className={'font-island text-5xl  p-2'}>
                     Task Hub
