ba613b490edfa0650242c4686c1bd1d4a207ffe8
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-05-11 14:28:06 -0500

    Shadcn ui integrate

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 import './components/Navbar.tsx'
 import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
   return (
     <>
      <Navbar />
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button>Click me</Button>
+        </div>
         <div className="uppersection flex text-5xl font-roboot font-semibold gap-2 p-6 w-full pl-25 ml-2">Hello,
         <div className="uppersection text-5xl font-roboot font-semibold opacity-60"> Start planning today</div> </div>
         <Footer />
