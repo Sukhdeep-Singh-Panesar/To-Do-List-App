1ad25e0d2f8ee3abba3adee395928e8d9b0fd9bd
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-05-11 14:12:36 -0500

    Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import './components/Navbar.tsx'
 import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
 
   return (
     <>
      <Navbar />
+        <div className="uppersection flex text-5xl font-roboot font-semibold gap-2 p-6 w-full pl-25 ml-2">Hello,
+        <div className="uppersection text-5xl font-roboot font-semibold opacity-60"> Start planning today</div> </div>
+        <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+
+function Footer() {
+    return (
+        <>
+            <div className=" flex w-full justify-between items-center bg-pink-200 gap-2 bottom-0 left-0 fixed p-7">
+                <div className="bottomleft">Image</div>
+                <div className="centerContainer">&copy; 2026 Sukhdeep Singh. All Rights Reserved. </div>
+                <div className="right flex gap-2">
+                <div className="bottomright">link</div>
+                <div className="bottomright">link</div>
+                <div className="bottomright">link</div>
+                <div className="bottomright">link</div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Footer
