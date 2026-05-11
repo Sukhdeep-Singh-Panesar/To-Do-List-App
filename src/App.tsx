4c856a231b4f83847787106aca83b7df9fd5c665
Sukhdeep Singh <sukhdeepsingh1821@gmail.com>
2026-05-11 15:37:17 -0500

    Adds placeholde and day setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import './App.css'
-import './components/Navbar.tsx'
 import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Card} from "@/components/ui/card.tsx";
 
 function App() {
+    const today:Date = new Date();
+
+    const day = today.toLocaleString("en-US",{
+        weekday: "long",
+    });
 
   return (
     <>
      <Navbar />
-        <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
-        </div>
+
+        {/*Upper Section*/}
         <div className="uppersection flex text-5xl font-roboot font-semibold gap-2 p-6 w-full pl-25 ml-2">Hello,
         <div className="uppersection text-5xl font-roboot font-semibold opacity-60"> Start planning today</div> </div>
+
+        <Card className="m-20 mt-2 bg-yellow-50">
+        {/*Middle Section*/}
+        <div className="middle flex gap-6 items-center justify-center p-6 w-full ml-2 ">
+            <div className="datecontainer text-3xl pr-20 font-semibold text-red-300">
+                {day}
+            </div>
+        <Input type={'text'} placeholder={'type Title of task'} className="h-15 text-sm w-64 ml-15 mr-3 bg-blue-100 placeholder:text-center"/>
+        <Input type={'text'} placeholder={'Detail of your task'} className="flex-1 h-15 text-sm bg-blue-100 placeholder:text-center"/>
+        <Button className={'bg-green-600 h-15 p-4'}>add</Button>
+        </div>
+            </Card>
+
         <Footer />
     </>
   )
